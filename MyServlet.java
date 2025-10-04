e2e5b3f248f9f0dba788ee8a681841b9f1c205a9
Christian Turean <christian.turean@ucdconnect.ie>
2025-10-04 08:05:15 +0100

    working

--- MyServlet.java
+++ MyServlet.java
@@ -1,10 +1,27 @@
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
 
+@WebServlet("/MyServlet")
 public class MyServlet extends HttpServlet {
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            // Retrieve input from HTML form
+            String userInput = request.getParameter("country");
+
+            // Assign it to a Java variable
+            String javaVariable = userInput;
+
+            // Send response back to client
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<h2>You entered: " + javaVariable + "</h2>");
+        } catch (Exception e) {
+            System.err.println("Error occurredbb: " + e.getMessage());
+            e.printStackTrace();
+    }
+            //throws ServletException, IOException {
         // need to install tomcat server to run this code
         // https://tomcat.apache.org/download-11.cgi
         // the binary -> core -> zipped
@@ -21,5 +38,7 @@ public class MyServlet extends HttpServlet {
         // response.setContentType("text/html");
         // PrintWriter out = response.getWriter();
         // out.println("<h2>You entered: " + javaVariable + "</h2>");
-    }
+    //}
+}
+
 }
